02aa07219fef261a82b77e067064bd3d6afc4e36
Michelwas <mgwassing@gmail.com>
2021-11-10 02:35:05 +0100

    added stuffs

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 var openButton = document.getElementById("open-button");
 
 function openMenu(){
-	let fieldset = document.querySelector("fieldset");
-	fieldset.classList.toggle("showFieldset");
+	let div = document.querySelector(".side-menu");
+	div.classList.toggle("showSideNav");
 }
 
 openButton.addEventListener("click", openMenu);
@@ -10,10 +10,30 @@ openButton.addEventListener("click", openMenu);
 var closeButton = document.getElementById("close-button");
 
 function closeMenu(){
-	let fieldset = document.querySelector("fieldset");
-	fieldset.classList.toggle("showFieldset");
+	let div = document.querySelector(".side-menu");
+	div.classList.toggle("showSideNav");
 }
 
 closeButton.addEventListener("click", closeMenu);
 
 // source: https://codepen.io/michel-wassing/pen/BaZeErj
+
+// TODO: make double column and single card view
+
+var graphButton = document.getElementById("graph-button");
+
+function graphView(){
+	let div = document.querySelector(".graph-view");
+	div.classList.toggle("showgraph");
+}
+
+graphButton.addEventListener("click", graphView);
+
+var columnButton = document.getElementById("column-button");
+
+function columnView(){
+	let div = document.querySelector(".column-view");
+	div.classList.toggle("showcolumn");
+}
+
+columnButton.addEventListener("click", columnView);
